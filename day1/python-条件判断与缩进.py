4ae8eac0d7dfcc66584ce0b7be26c0ed9ae54b4d
shichao <shichao@schxck.com>
2019-07-01 15:22:00 +0800

    提交python-条件判断与缩进

---
+++ day1/python-条件判断与缩进.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+
+sex = input("input is  name:" )
+if sex == "aa":
+    print("I name is aa")
+elif sex == "bb":
+    print("I name is bb")
+else:
+    print("I name None of the above is satisfactory. ")
+
