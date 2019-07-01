b1cccf4547998615cd22c763a976ec97fcf6b8c0
shichao <shichao@schxck.com>
2019-07-01 20:53:49 +0800

    提交python-装饰器

---
+++ day4/python-装饰器.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+#案例一
+def foo():
+    print("in the foo")
+    bar()
+def bar():
+    print("in the bar")
+foo()
+
+
+
+
+
