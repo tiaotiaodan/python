355abf10404cd6e7f2e2ee7425e6ced56377a12c
shichao <shichao@schxck.com>
2019-07-03 15:36:39 +0800

    day1-python注释

---
+++ day1/python-注释.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+#多行注释
+'''
+print("hello word!")
+
+'''
+
+
+#单行注释
+    # print("hello word!")
+
+
+print("hello word!")     #没有注释
