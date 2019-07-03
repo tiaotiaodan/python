e07886626b1d5fee7c671876609642d69c381ced
shichao <shichao@schxck.com>
2019-07-03 21:06:58 +0800

    day1-python字符串传参

---
+++ day1/python-用户输入-字符串传参.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+name = input("name:")
+age = input("age:")
+job = input("job:")
+salary = input("salary:")
+
+info = '''
+
+------------- info of  %s ------------
+Name: %s
+Age:%s
+Job:%s
+salary: %s
+
+'''   %(name,name,age,job,salary)
+
+print(info)
