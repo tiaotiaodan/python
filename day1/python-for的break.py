ea3f319e0ab944b0274e1c7d29011330793a093d
shichao <shichao@schxck.com>
2019-07-04 15:45:56 +0800

    day1-python for循环

---
+++ day1/python-for的break.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+# for i  in   range(10):
+#     if i < 5 :
+#         continue
+#     print("loop:", i)
+#
+
+
+for i  in   range(10):
+    if i == 5 :
+        break  #if判断等于5的时候，退出整个循环
+    print("loop:", i)
---
+++ day1/python-for的continue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author:shichao
+# File: .py
+
+# for i  in   range(10):
+#     if i < 5 :
+#         continue
+#     print("loop:", i)
+#
+
+
+for i  in   range(10):
+    if i == 5 :
+        continue  #if判断等于5的时候，退出本次循环，继续执行后面的操作
+    print("loop:", i)
