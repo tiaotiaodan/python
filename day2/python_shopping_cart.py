f30c53d62e8f7150518d3b739f9c851659b44b79
shichao <shichao@schxck.com>
2019-07-25 14:59:10 +0800

    python购物车连接

--- day2/python_shopping_cart.py
+++ day2/python_shopping_cart.py
@@ -11,3 +11,33 @@ product_list = [
     ('booke',20),
     ('Coffee',31),
 ]
+
+shopping_list = [ ]
+salary = input("请你输入数字：")
+
+if salary.isdigit():
+    salary = int(salary)
+    while True:
+        for index,item in enumerate(product_list):
+            print(index,item)
+        user_choice = input("选择要买什么东西？>>>:")
+        if user_choice.isdigit():
+            user_choice = int(user_choice)
+            if user_choice < len(product_list) and user_choice >= 0 :
+                p_item = product_list[user_choice]
+                if p_item[1] <= salary:
+                    shopping_list.append(p_item)
+                    salary -= p_item[1]
+                    print("Added %s into shopping cart ,your current balance is %s" %(p_item,salary) )
+                else:
+                    print("你余额不足只剩[%s]，请充值" %(salary))
+            else:
+                print("你输入该商品不存在!")
+        elif user_choice == 'q':
+            print("---------shopping list--------")
+            for p in shopping_list:
+                print(p)
+            print("你当前余额:",salary)
+            exit()
+else:
+    print("输入错误，请输入数字！")
